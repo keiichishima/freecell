9f88ce0f2b12357d5b621cda73a61605b3d14b78
Keiichi SHIMA <keiichi@kshima.org>
2026-06-25 18:51:38 +0900

    More tries
    Updated AppleTitle to use the help bundle identifier.
    Specified HPDBookAccessPath.
    
    with no luck...

---
+++ Freecell.help/Contents/Resources/mkidx.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for lc in en es fi fr ja nl
+do
+	(
+	cd ${lc}.lproj
+	hiutil -I lsm -Cf Freecell.helpindex -a -s ${lc} -l ${lc} .
+	hiutil -I corespotlight  -Cf Freecell.cshelpindex -a -s ${lc} -l ${lc} .
+	)
+done
